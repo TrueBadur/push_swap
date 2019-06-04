4b888438669283fd21b67cd61ada34bab235e25f
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-06-04 22:27:09 +0300

    Cleanup main: remove already unnecessary initialisations and put code for bubble sort to separate func
    Took 1 hour 53 minutes

--- push_swap/push_swap_main.c
+++ push_swap/push_swap_main.c
@@ -12,8 +12,6 @@
 
 #include "push_swap.h"
 
-
-
 int main(int ac, char **av)
 {
 	t_mngr	mngr;
@@ -22,22 +20,17 @@ int main(int ac, char **av)
 	if (ac == 1)
 		checker_error(NULL, NO_ARG);
 	else
-		parse_args(ac, av, &mngr); //TODO parse - arguments
-	mngr.vec = ft_vecinit(mngr.stk[0]->lst_s);
-	ft_avltovec(mngr.s_arr, mngr.vec);
+		parse_args(ac, av, &mngr);
+	if (mngr.bub)
+		set_for_bub(&mngr);
 	mngr.s_arr = ft_avlfree(mngr.s_arr);
 	if (check_sort(mngr.stk[0], ASC, 1) && !mngr.stk[1]->lst)
 		return (0);
-	set_sort(mngr.stk[0], ASC, mngr.stk[0]->max);
-	mngr.stk[0]->piv = ((int*)(mngr.vec->data))[(mngr.stk[0]->lst_s - 1) / 2];
-	mngr.stk[0]->max = ((int*)(mngr.vec->data))[mngr.stk[0]->lst_s - 1];
-	mngr.stk[0]->min = ((int*)(mngr.vec->data))[0];
-	mngr.dbg = 1;
-	mngr.col = 1;
-	mngr.n_cmd = 0;
 	draw_stacks(&mngr);
 	gen_commands(&mngr);
-	ft_printf("Total operations = %d\n", mngr.n_cmd);
-	return (check_sort(mngr.stk[0], ASC, 1) && !mngr.stk[1]->lst);
+	if (mngr.dbg)
+		ft_printf("Total operations = %d\n", mngr.n_cmd);
+	return ((check_sort(mngr.stk[0], ASC, 1) && !mngr.stk[1]->lst) ? 0 :
+	SORT_FAILED);
 }
 
